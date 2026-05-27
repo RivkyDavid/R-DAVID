ece463375331533579b789ab2a0ecb1b42574f4f
Rivky David <r0556770546@gmail.com>
2026-05-28 01:20:34 +0300

    add in ex 1 the second part

--- sync1.js
+++ sync1.js
@@ -1,25 +1,46 @@
+const { log } = require('console');
 const fs = require('fs');
 const path = require('path');
 
-const outputFileName = 'fileList.txt';
+const fileName = 'fileList.txt';
 const dirPath = './';
 let outputData = '';
 
 const files = fs.readdirSync(dirPath);
 
 files.forEach(file => {
-    if (file === outputFileName) return;
+    if (file === fileName) return;
 
     const fullPath = path.join(dirPath, file);
     const stats = fs.statSync(fullPath);
 
     if (stats.isFile()) {
         const content = fs.readFileSync(fullPath, 'utf8');
-        outputData += `Name: ${file}, Ext: ${path.extname(file)}, Modified: ${stats.mtime}\nContent: ${content}\n---\n`;
+        outputData += `Name: ${file}, EndName: ${path.extname(file)}, LstUpate: 
+        ${stats.mtime}\nContent: ${content}\n***\n`;
     } else if (stats.isDirectory()) {
-        outputData += `Directory: ${file}\nContents: ${fs.readdirSync(fullPath).join(', ')}\n---\n`;
+        outputData += `Directory: ${file}\nContents: ${fs.readdirSync(fullPath).join(', ')}
+        \n***\n`;
     }
 });
 
-fs.writeFileSync(outputFileName, outputData);
+fs.writeFileSync(fileName, outputData);
 console.log('Sync processing complete.');
+
+const target=process.argv[2];
+if(target){
+    if(!fs.existsSync(target)){
+    console.log("file not exist");  
+}else{
+    const stats=fs.statSync(target);
+
+    if(stats.isFile()){
+        fs.unlinkSync(target);
+        console.log(`file ${target} deleted successfully`);    
+    }else if(ststs.isDirectory()){
+        fs.rmSync(target,{recursive:true,force:true});
+        console.log(`diractory ${target} deleted successfully`);
+        
+    }
+}     
+}
